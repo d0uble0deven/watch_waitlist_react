584c91b400a0a7857b74006632ff8f8b5c20db9a
Dev Govindji <friendsofdev94@gmail.com>
2020-05-29 16:28:25 -0500

    refactors selection functionality in WatchCard

--- my-app/src/Components/WatchCard.jsx
+++ my-app/src/Components/WatchCard.jsx
@@ -8,6 +8,7 @@ import './ComponentStyling/WatchCardStyling.css'
 
 const WatchCard = () => {
     const [selected, setSelected] = useState([])
+    const [name, setName] = useState('')
 
     const watches = [
         { name: 'Cosmograph Daytona', image: daytona }, { name: 'Sky Dweller', image: skydweller }, { name: 'Datejust', image: datejust }, { name: 'Milgauss', image: milgauss }];
@@ -15,10 +16,10 @@ const WatchCard = () => {
     const listWatches = watches.map((item, index) =>
         <div className="watch_list" >
             <br />
-            <Button className="watch_button" key={index + 1} onClick={() => onCheckboxBtnClick(index + 1)} active={selected.includes(index + 1)}>
-                <Card className="watch_card" inverse key={index + 1} >
+            <Button className="watch_button" key={index} onClick={() => onWatchClick(item.name)} active={selected.includes(index)}>
+                <Card className="watch_card" inverse key={index} >
                     <CardImgOverlay>
-                        <CardTitle className="watch_title">{item.name}{index + 1}
+                        <CardTitle className="watch_title">{item.name}
                         </CardTitle>
                         <CardImg className="watch_image" width="10%" src={item.image} alt={item.name} />
                     </CardImgOverlay>
@@ -28,28 +29,34 @@ const WatchCard = () => {
 
     )
 
+    const onWatchClick = (input) => {
 
-    const selectAll = () => {
-        let num = 0
-        let empty = []
-        if (selected.length == watches.length) {
-            setSelected(empty)
-            console.log('selected is cleared: ' + selected)
-            console.log(selected)
+        console.log(`onWatchClick is firing ${input}`)
+        console.log(selected)
+        // filter selected for input 
+        // if arr.length == 0, the push input
+        // else remove element
+
+        const filterInput = selected.filter(element => element === input)
+
+        if (filterInput) {
+            selected.push(input)
+        } else {
+            // deselecting does not work correctly
+            setSelected(selected)
         }
-        else {
-            while (num < watches.length) {
-                selected.push(num)
-                num++
-                console.log(selected)
-                setSelected(selected)
-            }
+        // if (selected.filter(element => element === input)) {
+        //     selected.push(input)
+        // } else {
+        //     // find repeated element and remove it
+        //     const deselectedWatch = selected.filter(element => element == input)
+        //     console.log(deselectedWatch)
+        //     setSelected(deselectedWatch)
 
-        }
 
-    }
+        // }
 
-    const onCheckboxBtnClick = (input) => {
+        /*
         // finds index of input in state array
         const index = selected.indexOf(input);
         // does not matter what the first number is, "if" refers to index position
@@ -61,13 +68,12 @@ const WatchCard = () => {
             //and removes one space aka the number itself
             selected.splice(index, 1);
         }
+        */
         setSelected([...selected]);
     }
 
     return (
-        <div>
-            <Button className="watch_select_all" color="primary" onClick={selectAll}>Select All Watches</Button>
-            <br />
+        <div> {}
             {listWatches}
 
 
