093df656aca4913c43ef89faae4870483ede3b98
Dev Govindji <friendsofdev94@gmail.com>
2020-07-04 14:14:57 -0500

    testing heroku after removing /build from client gitignore

---
+++ my-app/build/precache-manifest.51d62b45ea88ddf410bd664a68f4b5ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599071c72731d248a30f51dfd0eb3102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1183ae1032305b516057",
+    "url": "/static/css/2.04327796.chunk.css"
+  },
+  {
+    "revision": "f277b79a013815064940",
+    "url": "/static/css/main.77fbfe79.chunk.css"
+  },
+  {
+    "revision": "1183ae1032305b516057",
+    "url": "/static/js/2.dcffdf9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dcffdf9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f277b79a013815064940",
+    "url": "/static/js/main.e0a00650.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
