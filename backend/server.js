0dc9f67a5c0774153bdb6209cf7556c599d37cc9
Dev Govindji <friendsofdev94@gmail.com>
2020-07-12 16:26:54 -0500

    changes get route in server for heroku4

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('my-app/build'))
     // my-app/build
 
-    // const index = path.join(__dirname, 'build', 'index.html')
-    const index = path.join(__dirname, '../my-app/build/index.html')
+    const index = path.join(__dirname, 'build', 'index.html')
+    // const index = path.join(__dirname, '../my-app/build/index.html')
     console.log(__dirname)
     app.get('*', function (req, res) {
         // res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
